38b3db03672f4f4291cf9268f0439c923084636b
Amanda Karoline <carolinoamandacs@gmail.com>
2019-12-02 17:06:35 -0300

    Adding router-view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-   <router-view />
+  <div>  
+    <div id="app">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-};
+  name: 'app'
+}
 </script>
-
-
