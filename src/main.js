8fb74070ce58381e4a9913f7dd07503bc2b71319
Amanda Karoline <carolinoamandacs@gmail.com>
2019-12-02 17:05:09 -0300

    import and create route object

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from '@/routes'
+import BootstrapVue from 'bootstrap-vue'
+import routes from './routes'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
 
 
 new Vue({
