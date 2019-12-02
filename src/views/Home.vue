6272b32ddec66576d96f249656896b57f8bc67e0
Amanda Karoline <carolinoamandacs@gmail.com>
2019-12-02 17:03:43 -0300

    view home creation

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<b-row> 
+    <b-col> <register> </register> </b-col>
+</b-row>
+    
+</template>
+
+<script>
+import Register from '@/components/Register.vue'
+
+export default {
+  name: 'Home',
+  components: {
+      Register
+  }
+}
+</script>
